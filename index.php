a5a898ebc101cd44b5ba11c417502ffe1477ca37
Amaya <42989748+the-amaya@users.noreply.github.com>
2020-10-02 09:32:57 -0400

    quick framework, no php yet
    just a quick framework. still needs php/mysql

---
+++ index.php
@@ -0,0 +1,46 @@
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Have The Dogs Been Fed?</title>
+<meta name="description" content="Have The Dogs Been Fed?" />
+<meta property="og:title" content="HTDBF?" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://wilkinsonfarm.org/dogs" />
+<meta property="og:image" content="" />
+<meta property="og:description" content="asking the important questions" />
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+</head>
+
+<div class="theme-switch-wrapper">
+    <label class="theme-switch" for="checkbox">
+        <input type="checkbox" id="checkbox" />
+        <div class="slider round"></div>
+  </label>
+  <em></em>
+</div>
+
+<body>
+	<table width="100%" border="0" cellpadding="0" cellspacing="0">
+		<tr>
+			<th></th>
+			<th>last fed</th>
+			<th></th>
+		<tr>
+			<td id="leftcell">Svedka</td>
+			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="rightcell">(button to record a feeding)</td>
+		</tr>
+		<tr>
+			<td id="leftcell">Sangria</td>
+			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="rightcell">(button to record a feeding)</td>
+		</tr>
+		<tr>
+			<td id="leftcell">Islay</td>
+			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="rightcell">(button to record a feeding)</td>
+		</tr>
+	</table>
+</body>
+
+<script src="/night.js"></script>
