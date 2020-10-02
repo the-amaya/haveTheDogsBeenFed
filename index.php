678bdd66e654844981ff51362ddabc59d7298897
Amaya <42989748+the-amaya@users.noreply.github.com>
2020-10-02 13:14:18 -0400

    started trying to parse data from the server
    it doesnt work, something is wrong with either the json array we are receiving it or how we are calling data in it. view output in your browser console to see issue

--- index.php
+++ index.php
@@ -9,6 +9,30 @@
 <meta property="og:description" content="asking the important questions" />
 <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+	function getData() {
+				var jsonData = $.ajax({
+					url: "readdb.php",
+					dataType: "json",
+					async: false
+				}).responseText;
+				console.log(jsonData);
+				data = jsonData[0][1];
+				console.log(data)
+	}
+
+
+	var inst = setInterval(update, 1000);
+	var elem = document.getElementById("id-1");
+
+
+	function update() {
+		getData()
+		elem.innerHTML = data;
+	}
+
+</script>
 </head>
 
 <div class="theme-switch-wrapper">
@@ -27,7 +51,7 @@
 			<th></th>
 		<tr>
 			<td id="leftcell">Svedka</td>
-			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="middlecell"><div id="id-1"></div></td>
 			<td id="rightcell">
 				<form action="writedb.php" method="post">
 					<input id="id" type="hidden" name="id" value="1">
