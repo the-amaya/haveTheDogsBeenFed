c928b316c3b63453ec399e2db2adac47a4d46e1f
Amaya <42989748+the-amaya@users.noreply.github.com>
2020-10-02 22:04:00 -0400

    last feed time now works
    we are now able to retrieve the last feed time and update a count-up for each dog to list how long it has been since they were last fed. css is not working for the counter currently

--- index.php
+++ index.php
@@ -10,29 +10,7 @@
 <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript">
-	function getData() {
-				var jsonData = $.ajax({
-					url: "readdb.php",
-					dataType: "json",
-					async: false
-				}).responseText;
-				console.log(jsonData);
-				data = jsonData[0][1];
-				console.log(data)
-	}
 
-
-	var inst = setInterval(update, 1000);
-	var elem = document.getElementById("id-1");
-
-
-	function update() {
-		getData()
-		elem.innerHTML = data;
-	}
-
-</script>
 </head>
 
 <div class="theme-switch-wrapper">
@@ -47,11 +25,22 @@
 	<table width="100%" border="0" cellpadding="0" cellspacing="0">
 		<tr>
 			<th></th>
-			<th>last fed</th>
+			<th>Time Since Last Fed</th>
 			<th></th>
 		<tr>
 			<td id="leftcell">Svedka</td>
-			<td id="middlecell"><div id="id-1"></div></td>
+			<td id="middlecell">
+				<div class="countup" id="countup1">
+					<span class="timeel days">00</span>
+					<span class="timeel timeRefDays">days</span>
+					<span class="timeel hours">00</span>
+					<span class="timeel timeRefHours">hours</span>
+					<span class="timeel minutes">00</span>
+					<span class="timeel timeRefMinutes">minutes</span>
+					<span class="timeel seconds">00</span>
+					<span class="timeel timeRefSeconds">seconds</span>
+				</div>
+			</td>
 			<td id="rightcell">
 				<form action="writedb.php" method="post">
 					<input id="id" type="hidden" name="id" value="1">
@@ -61,7 +50,18 @@
 		</tr>
 		<tr>
 			<td id="leftcell">Sangria</td>
-			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="middlecell">
+				<div class="countup" id="countup2">
+					<span class="timeel days">00</span>
+					<span class="timeel timeRefDays">days</span>
+					<span class="timeel hours">00</span>
+					<span class="timeel timeRefHours">hours</span>
+					<span class="timeel minutes">00</span>
+					<span class="timeel timeRefMinutes">minutes</span>
+					<span class="timeel seconds">00</span>
+					<span class="timeel timeRefSeconds">seconds</span>
+				</div>
+			</td>
 			<td id="rightcell">
 				<form action="writedb.php" method="post">
 					<input id="id" type="hidden" name="id" value="2">
@@ -71,7 +71,18 @@
 		</tr>
 		<tr>
 			<td id="leftcell">Islay</td>
-			<td id="middlecell">(read last time fed from database using php)</td>
+			<td id="middlecell">
+				<div class="countup" id="countup3">
+					<span class="timeel days">00</span>
+					<span class="timeel timeRefDays">days</span>
+					<span class="timeel hours">00</span>
+					<span class="timeel timeRefHours">hours</span>
+					<span class="timeel minutes">00</span>
+					<span class="timeel timeRefMinutes">minutes</span>
+					<span class="timeel seconds">00</span>
+					<span class="timeel timeRefSeconds">seconds</span>
+				</div>
+			</td>
 			<td id="rightcell">
 				<form action="writedb.php" method="post">
 					<input id="id" type="hidden" name="id" value="3">
@@ -81,5 +92,66 @@
 		</tr>
 	</table>
 </body>
+
+<script type="text/javascript">
+	function wrapper() {
+		function getData() {
+				var jsonData = $.ajax({
+					url: "readdb.php",
+					dataType: "json",
+					async: false
+				}).responseText;
+				jsonData = JSON.parse(jsonData);
+				console.log(jsonData);
+				data1 = jsonData[0]["time"];
+				data2 = jsonData[1]["time"];
+				data3 = jsonData[2]["time"];
+		};
+		
+		var inst = setInterval(update, 1000);
+		getData();
+
+		countUpFromTime(data1, 'countup1');
+		countUpFromTime(data2, 'countup2');
+		countUpFromTime(data3, 'countup3');
+
+		function update() {
+			getData();
+			
+			countUpFromTime(data1, 'countup1');
+			countUpFromTime(data2, 'countup2');
+			countUpFromTime(data3, 'countup3');
+		};
+	};
+	var elem1 = document.getElementById("dog1");
+	var elem2 = document.getElementById("dog2");
+	var elem3 = document.getElementById("dog3");
+	wrapper();
+
+	function countUpFromTime(countFrom, id) {
+		countFrom = new Date(countFrom).getTime();
+		var now = new Date(),
+			countFrom = new Date(countFrom),
+			timeDifference = (now - countFrom);
+
+		var secondsInADay = 60 * 60 * 1000 * 24,
+			secondsInAHour = 60 * 60 * 1000;
+
+		days = Math.floor(timeDifference / (secondsInADay) * 1);
+		hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+		mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+		secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+		var idEl = document.getElementById(id);
+		idEl.getElementsByClassName('days')[0].innerHTML = days;
+		idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+		idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+		idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+
+		clearTimeout(countUpFromTime.interval);
+		//countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+}
+
+</script>
 
 <script src="night.js"></script>
--- readdb.php
+++ readdb.php
@@ -14,14 +14,14 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 } 
 
-$jsonarray = [];
+$jsonarray = array();
 foreach ($rooms as $room) {
 	$sql = "SELECT id,time FROM dogs WHERE `id`='" . $room . "' ORDER BY `key` DESC LIMIT 1;";
 	$result = $conn->query($sql);
 	$row = $result->fetch_assoc();
-	$time = $row["time"];
-	$id = $row["id"];
-	$sarray = [$id, $time];
+	$sarray['id'] = $row["id"];
+	$sarray['time'] = $row["time"];
+	#$sarray = [$id, $time];
 	array_push($jsonarray, $sarray);
 	}
 
